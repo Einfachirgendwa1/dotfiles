4203db7862e60d256b5f92c9d3e243d4c8861e3a
Einfachirgendwa1 <faris@karge.berlin>
2024-06-08 07:13:36 +0200

    Mouse für neovim disabled

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -37,7 +37,9 @@ require("nvim-treesitter.configs").setup {
   },
 }
 
-vim.cmd "set clipboard=unnamed"
+vim.o.mouse = nil
+vim.o.clipboard = "unnamed"
+
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
