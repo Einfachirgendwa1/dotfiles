1afc5cbe568eb0ce5e965522abf1c881009761cf
Einfachirgendwa1 <faris@karge.berlin>
2024-06-09 10:19:37 +0200

    Spotify Interval auf 1 gesetzt

--- .config/waybar/mediaplayer.sh
+++ .config/waybar/mediaplayer.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-playerctl metadata title
+player_status=$(playerctl status 2>/dev/null)
+if [ "$player_status" = "Playing" ]; then
+	playerctl metadata title
+elif [ "$player_status" = "Paused" ]; then
+	echo " $(playerctl metadata title)"
+fi
