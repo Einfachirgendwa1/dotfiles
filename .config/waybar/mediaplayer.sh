3443adbcc0001185c73b760b837feb23a4a11527
Einfachirgendwa1 <faris@karge.berlin>
2024-06-15 08:21:31 +0200

    Spotify display zu audio player in general gemacht

--- .config/waybar/mediaplayer.sh
+++ .config/waybar/mediaplayer.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-player_status=$(playerctl status 2>/dev/null)
-if [ "$player_status" = "Playing" ]; then
-	playerctl metadata title
-elif [ "$player_status" = "Paused" ]; then
-	echo " $(playerctl metadata title)"
+
+player_status=$(playerctl status 2>&1)
+
+if [ "$player_status" = "No players found" ]; then
+	exit 1
 fi
+
+player_name=$(playerctl metadata | awk 'NR == 1 { print $1 }' 2>/dev/null)
+
+if [ "$player_name" = "spotify" ]; then
+	printf " "
+elif [ "$player_name" = "firefox" ]; then
+	printf "󰈹 "
+fi
+
+if [ "$player_status" = "Paused" ]; then
+	printf " "
+fi
+
+printf " "
+playerctl metadata title
+exit 0
