b1d3d2f320d7b378156147378152f3afaa3f7b05
Einfach_Irgendwa1 <faris.karge@berlin>
2024-08-19 19:04:11 +0200

    random bullshit go

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -18,24 +18,27 @@ require("neotest").setup {
 -- require("rust_analyzer_fast_display_thing").setup()
 require("autosave").setup()
 
----@diagnostic disable-next-line: missing-fields
-require("nvim-treesitter.configs").setup {
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    colors = {
-      "#f38ba8",
-      "#fab387",
-      "#f9e2af",
-      "#a6e3a1",
-      "#89dceb",
-      "#74c7ec",
-      "#89b4fa",
-      "#b4befe",
+local ft = vim.filetype.match { buf = 0 }
+if ft ~= "javascript" then
+  ---@diagnostic disable-next-line: missing-fields
+  require("nvim-treesitter.configs").setup {
+    rainbow = {
+      enable = true,
+      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      colors = {
+        "#f38ba8",
+        "#fab387",
+        "#f9e2af",
+        "#a6e3a1",
+        "#89dceb",
+        "#74c7ec",
+        "#89b4fa",
+        "#b4befe",
+      },
     },
-  },
-}
+  }
+end
 
 vim.o.mouse = ""
 vim.o.clipboard = "unnamed"
@@ -47,6 +50,47 @@ vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 vim.cmd.set "exrc"
 
+require("dap").configurations.rust = {
+  {
+    name = "Launch",
+    type = "codelldb",
+    request = "launch",
+    program = function() -- Ask the user what executable wants to debug
+      local handle =
+        io.popen "cargo build --message-format=json | jq -r 'select(.profile.test == false and .executable != null) | .executable' | grep '^/' 2>&1"
+      if handle == nil then
+        vim.notify "Failed to run cargo"
+        return
+      end
+      local result = handle:read "*l"
+      handle:close()
+      return result
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    args = {},
+    initCommands = function() -- add rust types support (optional)
+      -- Find out where to look for the pretty printer Python module
+      local rustc_sysroot = vim.fn.trim(vim.fn.system "rustc --print sysroot")
+
+      local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+      local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+      local commands = {}
+      local file = io.open(commands_file, "r")
+      if file then
+        for line in file:lines() do
+          table.insert(commands, line)
+        end
+        file:close()
+      end
+      table.insert(commands, 1, script_import)
+
+      return commands
+    end,
+  },
+}
+
 -- vim.opt.spell = true
 --
 -- vim.cmd [[
@@ -55,3 +99,9 @@ vim.cmd.set "exrc"
 --   autocmd FileType * syntax match SpellText /\v".{-}"/ contains=@Spell
 -- augroup END
 -- ]]
+--
+-- require("mason").setup {
+--   registries = {
+--     "file:~/dev/rust/asm_tools/mason-registry",
+--   },
+-- }
